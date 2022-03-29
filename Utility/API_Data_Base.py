df8806ebea9c386d45930e28f87873dd60a88a7a
Alexander <a.lyakh@g.nsu.ru>
2022-03-30 01:11:22 +0700

    API saves

--- Utility/API_Data_Base.py
+++ Utility/API_Data_Base.py
@@ -1,1 +1,12 @@
 token = "SCFFAtHIAFhTa4HOBoAvOgqMDqq8uOBd_1648434328"
+links = {
+    "faculty":"table.nsu.ru/api/faculty",
+    "teacher":"table.nsu.ru/api/teacher",
+    "group":"table.nsu.ru/api/group",
+    "time":"table.nsu.ru/api/time",
+    "type-occupation":"table.nsu.ru/api/type-occupation",
+    "parity":"table.nsu.ru/api/parity",
+    "term":"table.nsu.ru/api/term",
+    "exam-group":"table.nsu.ru/api/exam-group",
+    "schedule":"table.nsu.ru/api/schedule"
+}
