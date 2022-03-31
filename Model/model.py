d79bb2892d2f19d0a78459e0bba6ff2ab3dc92cc
Alexander <a.lyakh@g.nsu.ru>
2022-04-01 01:56:25 +0700

    Разработка моделей и правка API_Data_Base

---
+++ Model/model.py
@@ -0,0 +1,41 @@
+class Lesson():
+    # параметры для поиска:
+
+    # под преподавателем
+    id_teacher: str
+    id_time: str
+    weekday: str
+
+    # под студентом
+    # конкретно одна группа
+    id_list_group: list[str]
+
+    id_type_lesson: str
+    room: str
+
+    def __init__(self,**kwargs):
+        # self.id - не имеет смысла
+        self.id_teacher = id_teacher
+        self.id_type_lesson = id_type_lesson
+        self.weekday = weekday
+        self.id_list_group = id_groups
+        self.id_time = id_time
+        self.room = room
+
+class Shedule():
+    def __init__(self,):
+        # тут пока хз
+        # как расписание разбивать
+
+class Student():
+
+    def __init__(self, id, name, id_group, id_faculty):
+        self.id = id
+        self.id_group = id_group
+        self.id_faculty = id_faculty
+
+class Teacher():
+
+    def __init__(self, id, name):
+        self.id = id
+        # может ещё данные потребуются
--- Utility/API_Data_Base.py
+++ Utility/API_Data_Base.py
@@ -1,12 +1,13 @@
-token = "SCFFAtHIAFhTa4HOBoAvOgqMDqq8uOBd_1648434328"
+global token = "SCFFAtHIAFhTa4HOBoAvOgqMDqq8uOBd_1648434328"
 links = {
-    "faculty":"table.nsu.ru/api/faculty",
-    "teacher":"table.nsu.ru/api/teacher",
-    "group":"table.nsu.ru/api/group",
-    "time":"table.nsu.ru/api/time",
-    "type-occupation":"table.nsu.ru/api/type-occupation",
-    "parity":"table.nsu.ru/api/parity",
-    "term":"table.nsu.ru/api/term",
-    "exam-group":"table.nsu.ru/api/exam-group",
-    "schedule":"table.nsu.ru/api/schedule"
+    "faculty":"table.nsu.ru/api/faculties",
+    "teacher":"table.nsu.ru/api/teachers",
+    "group":"table.nsu.ru/api/groups",
+    "time":"table.nsu.ru/api/times",
+    "type-occupation":"table.nsu.ru/api/type-occupations",
+    "parity":"table.nsu.ru/api/parities",
+    "term":"table.nsu.ru/api/terms",
+    "exam-group":"table.nsu.ru/api/exam-groups",
+    "schedule":"table.nsu.ru/api/schedules",
 }
+# запрос links["<type_information>"] + "/search?" + ...
