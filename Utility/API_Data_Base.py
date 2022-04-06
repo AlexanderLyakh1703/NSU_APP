0135bc79cbeb642c98e85771c5b6bbcc1319b96a
Alexander <a.lyakh@g.nsu.ru>
2022-04-06 18:21:27 +0700

    Реализована авторизация на table.nsu.ru

--- Utility/API_Data_Base.py
+++ Utility/API_Data_Base.py
@@ -1,22 +1,43 @@
-import requests
+from http.client import HTTPSConnection
+from base64 import b64encode
+import json
 
-token = "SCFFAtHIAFhTa4HOBoAvOgqMDqq8uOBd_1648434328"
+# login:password
+AUTH = b"SCFFAtHIAFhTa4HOBoAvOgqMDqq8uOBd_1648434328:"
+
+# разделы API
 links = {
-    "faculty":"table.nsu.ru/api/faculties",
-    "teacher":"table.nsu.ru/api/teachers",
-    "group":"table.nsu.ru/api/groups",
-    "time":"table.nsu.ru/api/times",
-    "type-occupation":"table.nsu.ru/api/type-occupations",
-    "parity":"table.nsu.ru/api/parities",
-    "term":"table.nsu.ru/api/terms",
-    "exam-group":"table.nsu.ru/api/exam-groups",
-    "schedule":"table.nsu.ru/api/schedules",
+    "faculty":"/api/faculties",
+    "teacher":"/api/teachers",
+    "group":"/api/groups",
+    "time":"/api/times",
+    "type-occupation":"/api/type-occupations",
+    "parity":"/api/parities",
+    "term":"/api/terms",
+    "exam-group":"/api/exam-groups",
+    "schedule":"/api/schedules",
 }
 
-# запрос links["<type_information>"] + "/search?" + ...
+def connect(type :str, search :dict):
 
-def get_Time_By_IdTime(id_time :str):
-    link_request = links["time"] + "/search?" +
+    # заполнение данных дл авторизации
+    connect = HTTPSConnection("table.nsu.ru")
+    userAndPass = b64encode(bytes(AUTH)).decode("utf-8")
+    headers = { "Authorization" : "Basic %s" %  userAndPass }
 
-def get_smb():
-    ...
+    # собираем строку поиска
+    string_of_find = "&".join([elem+"="+search[elem] for elem in search.keys()])
+
+    # посылаем запрос
+    connect.request('GET', links[type]+"/search?"+string_of_find, headers=headers)
+    res = connect.getresponse()
+    data = res.read()
+
+    # возвращаем ответ в виде массива словарей
+    return json.loads(data.decode("utf-8"))
+
+# def get_Time_By_IdTime(id_time :str):
+#
+#
+# def get_smb():
+#     ...
