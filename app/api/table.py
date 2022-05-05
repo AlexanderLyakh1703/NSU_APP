99035e31e4d8ac9b4e5532c74f05740fb4d05be2
AlexanderLyakh1703 <a.lyakh@g.nsu.ru>
2022-05-05 23:44:51 +0700

    Checking new Function of API is info_for_Timetable

--- app/api/table.py
+++ app/api/table.py
@@ -40,5 +40,79 @@ def connect(type_: str, search=dict()) -> list[dict]:
     # возвращаем ответ в виде массива словарей
     return json.loads(data.decode("utf-8"))
 
-
 # example: print(connect("faculty",{"id":"1"}))
+
+def info_for_Timetable():
+    # get User from variable of session
+    User = session["User"]
+
+    # get the parity of the week
+    even = api.table.connect("parity")["actual"] # "odd" or "even"
+
+    if User.roles == "student":
+
+        # get id of group our User
+        id_group = api.table.connect("group",{"name":User.group})[0]["id"]
+
+        req_for_table = api.table.connect("schedule",{"id_group":id_group})
+
+        # make array of lessons
+        array_of_lessons = []
+
+        for row in req_for_lessons:
+            lesson = Lesson(row)
+            array_of_lessons.append(lesson)
+
+        order = Order(array_of_lessons)
+
+
+    elif User.roles == "teacher":
+
+        # we must swap all lessons with different id_groups and equal other params
+        req_for_table = api.table.connect("schedule",{"id_teacher":id_teacher})
+
+        # function for check equals of lessons
+        present = lambda write:[write["weekday"],
+                                write["id_time"],
+                                write["roomsokr"]]
+
+        req_for_table.sort(key=present)
+
+        number_write = 0
+
+        while number_write+1 < len(req_for_table):
+
+            this_write = req_for_table[number_write]
+            next_write = number_writes[number_write+1]
+
+            if present(this_write) == present(next_write):
+
+                    # i am writting here swap two objects
+                    if this_write['id_groups'] is list:
+                        this_write['id_groups'].append(next_write['id_groups'])
+                    else:
+                        this_write['id_groups'] = [this_write['id_groups'],
+                                                   next_write['id_groups'] ]
+                    del req_for_table[number_write+1]
+            else:
+                number_write += 1
+
+        # make array of lessons
+        array_of_lessons = []
+
+        for row in req_for_lessons:
+            lesson = Lesson(row)
+            array_of_lessons.append(lesson)
+
+        order = Order(array_of_lessons)
+
+    elif User.roles == "combo":
+        pass
+        # it's combination of "teacher" ahd "student"
+        # i don't smt about this
+
+    else:
+        pass
+        # ERROR: I don't know your role...
+
+    return render_template("Timetable.html",table=order,even=even,weekday=datetime.datetime.today().weekday()+1)
