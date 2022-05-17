7bd989c536b1b7bc2a42da57e1482e4dbf36fa9f
dsuh0i <dsuh0i.h8@gmail.com>
2022-05-18 00:16:39 +0700

    Delete useless rows in views.py.

--- app/api/table.py
+++ app/api/table.py
@@ -8,7 +8,7 @@ from flask import session
 from datetime import datetime
 
 # импоритруем модели
-from model import Order, Lesson
+from ..model import Order, Lesson
 
 # -----------------------------------------------------------------------------
 # разделы БД
@@ -47,34 +47,38 @@ def connect(type_: str, search=dict()) -> list[dict]:
     # возвращаем ответ в виде массива словарей
     return json.loads(data.decode("utf-8"))
 
+
 # example: print(connect("faculty",{"id":"1"}))
 
 
 # function for sortings and check lessons
-def check_for_equals(write :dict()):
-    return [ write["weekday"], write["id_time"] ]
+def check_for_equals(write: dict()):
+    return [write["weekday"], write["id_time"]]
+
 
 # объединяем пары с одинаковым днём недели, временем и преподавателем
-def union_of_lessons_by_groups(request_for_timetable :list[dict]): # for teachers
+def union_of_lessons_by_groups(request_for_timetable: list[dict]):  # for teachers
 
     request_for_timetable.sort(key=check_for_equals)
 
     number_write = 0
 
-    while number_write+1 < len(request_for_timetable):
+    while number_write + 1 < len(request_for_timetable):
 
         this_write = request_for_timetable[number_write]
-        next_write = number_writes[number_write+1]
+        next_write = number_writes[number_write + 1]
 
         if present(this_write) == present(next_write):
 
-                # i am writting here swap two objects
-                if this_write['id_groups'] is list:
-                    this_write['id_groups'].append(next_write['id_groups'])
-                else:
-                    this_write['id_groups'] = [this_write['id_groups'],
-                                               next_write['id_groups'] ]
-                del request_for_timetable[number_write+1]
+            # i am writting here swap two objects
+            if this_write["id_groups"] is list:
+                this_write["id_groups"].append(next_write["id_groups"])
+            else:
+                this_write["id_groups"] = [
+                    this_write["id_groups"],
+                    next_write["id_groups"],
+                ]
+            del request_for_timetable[number_write + 1]
         else:
             number_write += 1
 
@@ -89,25 +93,29 @@ def union_of_lessons_by_groups(request_for_timetable :list[dict]): # for teacher
 
     return order
 
+
 # получаем роль студент/преподаватель
 def get_roles(user):
 
-    fullname = ' '.join(list(user['name'].split()[::-1]
-                                + [user['middlename']]))
+    fullname = " ".join(list(user["name"].split()[::-1] + [user["middlename"]]))
 
     # не, тут явно уязвимость, т. к. люди с одинаковым ФИО будут учителями
-    id_teacher = connect("teacher",{'fullname':fullname})
+    id_teacher = connect("teacher", {"fullname": fullname})
 
     # есть ровно ОДНА запись с таким fullname И есть он НЕ бакалавр
-    if len(id_teacher) == 1 and user['groups']['StudentCourses'].split('_')[-1] != 'bac':
-        roles = 'teacher'
+    if (
+        len(id_teacher) == 1
+        and user["groups"]["StudentCourses"].split("_")[-1] != "bac"
+    ):
+        roles = "teacher"
     else:
-        roles = 'student'
+        roles = "student"
 
     return roles
 
+
 # конвертируем массив словарей в тип данных order
-def convert(list_of_dicts :list[dict]):
+def convert(list_of_dicts: list[dict]):
 
     array_of_lessons = []
 
@@ -124,28 +132,23 @@ def info_for_Timetable(session):
     user = session["userinfo"]
 
     # get the parity of the week
-    even = connect("parity")["actual"] # "odd" or "even"
+    even = connect("parity")["actual"]  # "odd" or "even"
 
-    weekday = datetime.today().weekday()+1 # value from {1,...,7}
-
+    weekday = datetime.today().weekday() + 1  # value from {1,...,7}
 
     roles = get_roles(user)
 
+    if roles == "student":
 
-    if roles == 'student':
+        id_group = connect("group", {"name": group})[0]["id"]
 
-        id_group = connect("group",{'name':group})[0]['id']
-
-        timetable = connect("schedule",{'id_group':id_group})
+        timetable = connect("schedule", {"id_group": id_group})
 
         order = convert(timetable)
 
-    else: # roles = 'teacher'
+    else:  # roles = 'teacher'
 
-        timetable = connect("schedule",{'id_teacher':id_teacher})
+        timetable = connect("schedule", {"id_teacher": id_teacher})
         order = convert(union_of_lessons_by_groups(timetable))
 
-
-
-    return {'timetable':order,'even':even,
-            'weekday':weekday,'roles':roles}
+    return {"timetable": order, "even": even, "weekday": weekday, "roles": roles}
--- app/main/views.py
+++ app/main/views.py
@@ -3,33 +3,26 @@ from flask import jsonify, redirect, render_template, request, session, url_for
 from ..api import auth, table
 from . import main
 
+
 @main.route("/")
 @main.route("/index")
 def index():
     return render_template("index.html")
 
 
-def get_auth(state=None, token=None):
-    if token:
-        return OAuth2Session(auconf.CLIENT_ID, token=token)
-    if state:
-        return OAuth2Session(
-            auconf.CLIENT_ID, state=state, redirect_uri=auconf.REDIRECT_URI
-        )
-    return OAuth2Session(
-        auconf.CLIENT_ID, redirect_uri=auconf.REDIRECT_URI, scope=auconf.SCOPE
-    )
-
 @main.route("/timetable")
 def timetable():
 
     dict_of_vars = table.info_for_Timetable(session)
 
-    return render_template("options/Timetable.html",
-                        table = dict_of_vars['timetable'],
-                        even = dict_of_vars['even'],
-                        weekday = dict_of_vars['weekday'],
-                        roles = dict_of_vars['roles'] )
+    return render_template(
+        "options/Timetable.html",
+        table=dict_of_vars["timetable"],
+        even=dict_of_vars["even"],
+        weekday=dict_of_vars["weekday"],
+        roles=dict_of_vars["roles"],
+    )
+
 
 @main.route("/login")
 def login():
